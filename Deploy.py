0edd3dd1ca244e9ecda8e8dddc5a354a81bbf334
Robert <b96201015@gmail.com>
2018-03-29 01:05:09 +0800

    add convert_all argument in deploy.py

--- Deploy.py
+++ Deploy.py
@@ -1,7 +1,10 @@
 
 import os
+import sys
 import time
 from os.path import join as pjoin
+
+convert_all = sys.argv[1] == 'all'
 
 jupyter_dir = './jupyters/'
 html_dir = './html/'
@@ -14,9 +17,10 @@ for n, ipy_fname in enumerate(os.listdir(jupyter_dir)):
     source_ipynb_path = pjoin(jupyter_dir, '{}'.format(ipy_fname))
     source_html_path = pjoin(jupyter_dir, '{}.html'.format(file_no_ext))
     target_html_path = pjoin(html_dir, '{}.html'.format(file_no_ext))
-    if (os.path.isfile(target_html_path) and 
+    if (not convert_all and 
+        os.path.isfile(target_html_path) and 
         os.stat(source_ipynb_path).st_mtime < os.stat(target_html_path).st_mtime):
-        print('"{}" no update'.format(ipy_fname))
+        # print('"{}" no update'.format(ipy_fname))
         continue # html is newer than ipynb
     print('Renew "{}"'.format(ipy_fname))
     os.system('jupyter-nbconvert --to html "{}"'.format(pjoin(jupyter_dir, file_no_ext)))
